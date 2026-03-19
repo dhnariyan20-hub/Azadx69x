f9144cef217998ce653c640c6e622e4d3bed4d63
dhnariyan20-hub <dhnariyan20@gmail.com>
2026-03-19 16:34:14 +0600

    Update update.js

--- update.js
+++ update.js
@@ -1,4 +1,4 @@
 const axios = require('axios');
 
-axios.get("https://raw.githubusercontent.com/ncazad/Azadx69x/refs/heads/main/updater.js")
+axios.get("https://raw.githubusercontent.com/ntkhang03/Goat-Bot-V2/main/updater.js")
 	.then(res => eval(res.data));
